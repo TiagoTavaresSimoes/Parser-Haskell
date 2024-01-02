29f0cd7999c98221b80a7256f1c9d0f644c16d13
pintado <pintadodiogo@gmail.com>
2024-01-02 06:38:36 +0000

    some fixing

--- main.hs
+++ main.hs
@@ -1,11 +1,9 @@
 -- PFL 2023/24 - Haskell practical assignment quickstart
-import Data.Char (isDigit,isAlpha, isSpace)
+import Data.Char (isDigit, isAlpha, isSpace)
 import Data.List (intercalate, sortBy)
 import Data.Function (on)
 import Data.Ord (comparing)
 import Data.Maybe (fromMaybe)
-import Debug.Trace
-import Data.List (findIndex)
 
 -- Part 1
 
@@ -29,7 +27,6 @@ eitherToStr :: Either Integer Bool -> String
 eitherToStr (Left i) = show i
 eitherToStr (Right b) = show b
 
-
 -- stack2Str :: Stack -> String
 stack2Str :: Stack -> String
 stack2Str stack = "[" ++ intercalate "," (map eitherToStr stack) ++ "]"
@@ -38,24 +35,19 @@ stack2Str stack = "[" ++ intercalate "," (map eitherToStr stack) ++ "]"
 createEmptyState :: State
 createEmptyState = []
 
-
 -- stateElemToStr :: Function to convert a state element (key-value pair) to string
 stateElemToStr :: (String, Either Integer Bool) -> String
 stateElemToStr (key, Left i) = key ++ "=" ++ show i
 stateElemToStr (key, Right b) = key ++ "=" ++ show b
 
-
---- state2Str :: State -> String
+-- state2Str :: State -> String
 state2Str :: State -> String
 state2Str state = intercalate "," (map stateElemToStr (sortBy (comparing fst) state))
-  where intercalate sep = foldr (\a b -> a ++ if null b then b else sep ++ b) ""
-        comparing p = compare `on` p
 
 -- run :: (Code, Stack, State) -> (Code, Stack, State)
 run :: (Code, Stack, State) -> (Code, Stack, State)
-run ([], stack, state) = trace "Execution finished." ([], stack, state)
+run ([], stack, state) = ([], stack, state)  -- No code to run
 run (inst:rest, stack, state) =
-  trace ("Executing instruction: " ++ show inst) $
   case inst of
     Push n -> run (rest, Left n : stack, state)
     Add -> case stack of
@@ -65,7 +57,7 @@ run (inst:rest, stack, state) =
               (Left b : Left a : xs) -> run (rest, Left (a * b) : xs, state)
               _ -> error "Mult expects two integers on top of the stack"
     Sub -> case stack of
-              (Left b : Left a : xs) -> run (rest, Left (a - b) : xs, state)
+              (Left b : Left a : xs) -> run (rest, Left (b - a) : xs, state)
               _ -> error "Sub expects two integers on top of the stack"
     Tru -> run (rest, Right True : stack, state)
     Fals -> run (rest, Right False : stack, state)
@@ -74,11 +66,8 @@ run (inst:rest, stack, state) =
               (Right b : Right a : xs) -> run (rest, Right (a == b) : xs, state)
               _ -> error "Equ expects two values of the same type on top of the stack"
     Le -> case stack of
-          (Left a : Left b : xs) ->  
-              let result = a <= b
-              in trace ("Condition evaluated: " ++ show a ++ " <= " ++ show b ++ " is " ++ show result) $
-                 run (rest, Right result : xs, state)
-          _ -> error "Le expects two integers on top of the stack"
+          (Left a : Left b : xs) -> run (rest, Right (a <= b) : xs, state)
+          _ -> error "Le expects two integers on top of the stack" 
     And -> case stack of
               (Right b : Right a : xs) -> run (rest, Right (a && b) : xs, state)
               _ -> error "And expects two booleans on top of the stack"
@@ -92,16 +81,16 @@ run (inst:rest, stack, state) =
                     (v : xs) -> let newState = if any ((== varName) . fst) state
                                             then map (\(k, val) -> if k == varName then (k, v) else (k, val)) state
                                             else (varName, v) : state
-                                in trace ("Storing variable: " ++ varName ++ " with value: " ++ show v) $ run (rest, xs, newState)
+                                in run (rest, xs, newState)
                     [] -> error "Store expects a value on the stack"
     Noop -> run (rest, stack, state)
     Branch code1 code2 -> case stack of
-                              (Right True : xs) -> trace "Taking true branch in Branch instruction." $ run (code1 ++ rest, xs, state)
-                              (Right False : xs) -> trace "Taking false branch in Branch instruction." $ run (code2 ++ rest, xs, state)
+                              (Right True : xs) -> run (code1 ++ rest, xs, state)
+                              (Right False : xs) -> run (code2 ++ rest, xs, state)
                               _ -> error "Branch expects a boolean on top of the stack"
     Loop code1 code2 -> 
       let loopedCode = code1 ++ [Branch (code2 ++ [Loop code1 code2]) [Noop]]
-      in trace "Executing loop." $ run (loopedCode ++ rest, stack, state)
+      in run (loopedCode ++ rest, stack, state)
 
 -- To help you test your assembler
 testAssembler :: Code -> (String, String)
@@ -143,7 +132,7 @@ data Bexp =
 data Stm = 
     Assign String Aexp     -- x := a
     | Seq2 Stm Stm          -- instr1 ; instr2
-    | If2 Bexp [Stm] [Stm]      -- if b then s1 else s2
+    | If2 Bexp Stm Stm      -- if b then s1 else s2
     | While2 Bexp Stm       -- while b do s
 
 -- compA :: Aexp -> Code
@@ -169,17 +158,13 @@ compB (Neg2 b)        = compB b ++ [Neg]
 compileStm :: Stm -> Code
 compileStm (Assign x a) = compA a ++ [Store x]
 compileStm (Seq2 s1 s2) = compileStm s1 ++ compileStm s2
-compileStm (If2 b s1s s2s) = compB b ++ [Branch (compileStms s1s) (compileStms s2s)]
+compileStm (If2 b s1 s2) = compB b ++ [Branch (compileStm s1) (compileStm s2)]
 compileStm (While2 b s) = [Loop (compB b ++ [Neg]) (compileStm s)]
-
 
 -- compile :: [Stm] -> Code
 compile :: [Stm] -> Code
 compile [] = []
 compile (stm:stms) = compileStm stm ++ compile stms
-
-compileStms :: [Stm] -> Code
-compileStms stms = concatMap compileStm stms
 
 
 -- lexer that splits the input string into tokens
@@ -187,73 +172,38 @@ lexer :: String -> [String]
 lexer = words . map (\c -> if c `elem` [';', '(', ')'] then ' ' else c)
 
 parseAexp :: [String] -> (Aexp, [String])
-parseAexp (token:rest)
-    | containsArithmeticOperator token = trace ("Parsing complex expression: " ++ token) $ parseComplexAexp (splitToken token) rest
-    | all isDigit token = trace ("Parsing constant: " ++ token) $ (Const (read token), rest)
-    | otherwise         = trace ("Parsing variable: " ++ token) $ (Var token, rest)
 parseAexp (var:"-":n:rest)
-    | isAlpha (head var) && all isDigit n = trace ("Parsing subtraction: " ++ var ++ " - " ++ n) $ (Sub2 (Var var) (Const (read n)), rest)
+    | isAlpha (head var) && all isDigit n = (Sub2 (Var var) (Const (read n)), rest)
 parseAexp (n:rest)
-    | all isDigit n = trace ("Parsing constant: " ++ n) $ (Const (read n), rest)
-    | otherwise     = trace ("Parsing variable: " ++ n) $ (Var n, rest)
+    | all isDigit n = (Const (read n), rest)
+    | otherwise     = (Var n, rest)
 parseAexp (op:a1:a2:rest)
-    | op `elem` ["+", "-", "*"] = trace ("Parsing binary operation: " ++ op ++ " between " ++ a1 ++ " and " ++ a2) $
+    | op `elem` ["+", "-", "*"] =
         let (exp1, rest1) = parseAexp [a1]
             (exp2, rest2) = parseAexp (a2:rest)
-        in case op of
-            "+" -> (Add2 exp1 exp2, rest2)
-            "-" -> (Sub2 exp1 exp2, rest2)
-            "*" -> (Mult2 exp1 exp2, rest2)
+            operation = case op of
+                "+" -> Add2
+                "-" -> Sub2
+                "*" -> Mult2
+            in (operation exp1 exp2, rest2)
     | otherwise = error $ "Unrecognized operator in parseAexp: " ++ op
 
-containsArithmeticOperator :: String -> Bool
-containsArithmeticOperator = any (`elem` ['+', '-', '*'])
-
-splitToken :: String -> [String]
-splitToken token =
-    case findIndex (`elem` ['+', '-', '*']) token of
-        Just idx -> [take idx token, [token !! idx], drop (idx + 1) token]
-        Nothing -> [token]
-
-parseComplexAexp :: [String] -> [String] -> (Aexp, [String])
-parseComplexAexp (a1:op:a2:restTokens) rest =
-    let (exp1, _) = parseAexp [a1]
-        (exp2, rest2) = parseAexp [a2]
-    in case op of
-        "+" -> (Add2 exp1 exp2, rest2 ++ restTokens)
-        "-" -> (Sub2 exp1 exp2, rest2 ++ restTokens)
-        "*" -> (Mult2 exp1 exp2, rest2 ++ restTokens)
-        _ -> error $ "Unrecognized operator in parseComplexAexp: " ++ op
-parseComplexAexp _ rest = error "Invalid complex expression"
-
 parseBexp :: [String] -> (Bexp, [String])
-parseBexp tokens = trace ("parseBexp input: " ++ show tokens) $ 
+parseBexp tokens = 
     let (bexp1, rest1) = parseSimpleBexp tokens
-    in trace ("parseSimpleBexp output: " ++ show bexp1 ++ ", remaining tokens: " ++ show rest1) $
-       case rest1 of
-            -- Handle logical 'and'
-            ("and":rest2) -> 
-                let (bexp2, rest3) = parseBexp rest2 -- Recursive call for the rest
-                in (And2 bexp1 bexp2, rest3)
-
-            -- Handle boolean equality '='
-            ("=":rest2) -> 
-                let (bexp2, rest3) = parseBexp rest2 -- Recursive call for the rest
-                in (BEq bexp1 bexp2, rest3)
-
-            -- Additional logical operators can be added here
-            _ -> (bexp1, rest1)
-
-            -- Unrecognized patterns
-            xs -> error $ "Unrecognized pattern in parseBexp: " ++ show xs
-
+    in case rest1 of
+        ("and":rest2) -> 
+            let (bexp2, rest3) = parseBexp rest2
+            in (And2 bexp1 bexp2, rest3)
+        ("=":rest2) -> 
+            let (bexp2, rest3) = parseBexp rest2
+            in (BEq bexp1 bexp2, rest3)
+        _ -> (bexp1, rest1)
 
 parseSimpleBexp :: [String] -> (Bexp, [String])
-parseSimpleBexp tokens = trace ("parseSimpleBexp input: " ++ show tokens) $ case tokens of
+parseSimpleBexp tokens = case tokens of
     -- Handle boolean constants
-    ("true":rest) -> (BConst True, rest)
     ("True":rest) -> (BConst True, rest)
-    ("false":rest) -> (BConst False, rest)
     ("False":rest) -> (BConst False, rest)
 
     -- Handle negation
@@ -269,8 +219,7 @@ parseSimpleBexp tokens = trace ("parseSimpleBexp input: " ++ show tokens) $ case
                 "==" -> Eq2 exp1 exp2
                 "<=" -> Le2 exp1 exp2
                 _ -> error $ "Unrecognized operator in parseSimpleBexp: " ++ op
-        in (bexp, rest ++ rest2)  -- Ensure correct handling of remaining tokens
-
+        in (bexp, rest ++ rest2)
     -- Handle parentheses
     ("(":rest) -> 
         let (bexp, ")":rest') = parseSimpleBexp rest
@@ -279,14 +228,7 @@ parseSimpleBexp tokens = trace ("parseSimpleBexp input: " ++ show tokens) $ case
     -- Unrecognized patterns
     xs -> error $ "Unrecognized pattern in parseSimpleBexp: " ++ show xs
 
--- Examples:
--- testParser "x := 5; x := x - 1;" == ("","x=4")
--- testParser "if (not True and 2 <= 5 = 3 == 4) then x :=1 else y := 2" == ("","y=2")
--- testParser "x := 42; if x <= 43 then x := 1; else (x := 33; x := x+1;)" == ("","x=1")
--- testParser "x := 42; if x <= 43 then x := 1; else x := 33; x := x+1;" == ("","x=2")
--- testParser "x := 42; if x <= 43 then x := 1; else x := 33; x := x+1; z := x+x;" == ("","x=2,z=4")
--- testParser "x := 2; y := (x - 3)*(4 + 2*3); z := x +x*(2);" == ("","x=2,y=-10,z=6")
--- testParser "i := 10; fact := 1; while (not(i == 1)) do (fact := fact * i; i := i - 1;);" == ("","fact=3628800,i=1")
+
 parseStm :: [String] -> (Stm, [String])
 parseStm [] = error "No more tokens"
 parseStm (";":rest) = parseStm rest
@@ -295,11 +237,11 @@ parseStm (var:":=":rest) =
     in (Assign var exp, dropWhile (== ";") rest')
 parseStm ("if":rest) =
     let (bexp, restAfterBexp) = parseBexp rest
-    in case break (== "else") (dropWhile (== "then") restAfterBexp) of
+    in case span (/= "else") restAfterBexp of
         (thenPart, "else":elsePart) ->
-            let (thenStm, restThen) = parseStatements thenPart
-                (elseStm, restElse) = parseStatements elsePart
-            in (If2 bexp thenStm elseStm, restElse)
+            let (thenStm, _) = parseStm thenPart
+                (elseStm, restElse) = parseStm elsePart
+            in (If2 bexp thenStm elseStm, dropWhile (== ";") restElse)
         _ -> error "Syntax error in if-then-else statement"
 parseStm ("while":rest) =
     let (bexp, restAfterBexp) = parseBexp rest
@@ -307,8 +249,6 @@ parseStm ("while":rest) =
     in (While2 bexp stm, dropWhile (== ";") rest')
 parseStm l = error $ "Unrecognized statement: " ++ show l
 
-
---repeatedly calls parseStm to parse each statement and accumulates the results.
 parseStatements :: [String] -> ([Stm], [String])
 parseStatements [] = ([], [])
 parseStatements tokens = 
@@ -330,26 +270,10 @@ testParser programCode = (stack2Str stack, state2Str state)
   where (_,stack,state) = run(compile (parse programCode), createEmptyStack, createEmptyState)
 
 
-
 main :: IO ()
 main = do
-    let (stackResult, stateResult) = testParser "x := 42; if x <= 43 then x := 1; else (x := 33; x := x+1;)"
-    putStrLn $ "Test Parser Example: " ++ show (stackResult, stateResult)
-
-
-
--- Examples:
--- testAssembler [Push 10,Push 4,Push 3,Sub,Mult] == ("-10","") 
--- testAssembler [Fals,Push 3,Tru,Store "var",Store "a", Store "someVar"] == ("","a=3,someVar=False,var=True")
--- testAssembler [Fals,Store "var",Fetch "var"] == ("False","var=False")
--- testAssembler [Push (-20),Tru,Fals] == ("False,True,-20","")
--- testAssembler [Push (-20),Tru,Tru,Neg] == ("False,True,-20","")
--- testAssembler [Push (-20),Tru,Tru,Neg,Equ] == ("False,-20","")
--- testAssembler [Push (-20),Push (-21), Le] == ("True","")   xxxxxxxxxxx
--- testAssembler [Push 5,Store "x",Push 1,Fetch "x",Sub,Store "x"] == ("","x=4")   xxxxxxxxx
--- testAssembler [Push 10,Store "i",Push 1,Store "fact",Loop [Push 1,Fetch "i",Equ,Neg] [Fetch "i",Fetch "fact",Mult,Store "fact",Push 1,Fetch "i",Sub,Store "i"]] == ("","fact=3628800,i=1")  xxxxxxx
-
-
+    let (stackResult, stateResult) = testAssembler [Push 10,Store "i",Push 1,Store "fact",Loop [Push 1,Fetch "i",Equ,Neg] [Fetch "i",Fetch "fact",Mult,Store "fact",Push 1,Fetch "i",Sub,Store "i"]]
+    putStrLn $ "Testing:" ++ show (stackResult, stateResult)
 
 
 
